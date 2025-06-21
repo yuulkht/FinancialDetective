614ad5a3bbe5f007d685baa3b5200351d4086f6d
yuulkht <juliakukhtina12@gmail.com>
2025-06-21 03:46:05 +0300

    fix: fix hardcode account id

--- app/src/main/java/ru/hse/financialdetective/data/mapper/AccountDtoToAccount.kt
+++ app/src/main/java/ru/hse/financialdetective/data/mapper/AccountDtoToAccount.kt
@@ -4,9 +4,8 @@ import ru.hse.financialdetective.data.model.AccountDto
 import ru.hse.financialdetective.domain.model.Account
 
 fun AccountDto.toDomain(): Account =
-    //TODO ХАРДКОД убрать id
     Account(
-        id = 1,//this.id,
+        id = this.id,
         name = this.name,
         balance = this.balance,
         currency = this.currency
