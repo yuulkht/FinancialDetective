b37af906c857298252286e91548cdca3852a9e64
yuulkht <juliakukhtina12@gmail.com>
2025-06-27 22:02:45 +0300

    fix: move date formatting to extension function

--- app/src/main/java/ru/hse/financialdetective/ui/uimodel/model/ExpensesWithTotalUiModel.kt
+++ app/src/main/java/ru/hse/financialdetective/ui/uimodel/model/ExpensesWithTotalUiModel.kt
@@ -3,7 +3,7 @@ package ru.hse.financialdetective.ui.uimodel.model
 import androidx.compose.runtime.Immutable
 
 sealed interface ExpensesUiState {
-    object Loading : ExpensesUiState
+    data object Loading : ExpensesUiState
     data class Success(val data: ExpensesWithTotalUiModel) : ExpensesUiState
     data class Error(val message: String) : ExpensesUiState
 }
